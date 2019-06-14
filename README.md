cddd6d9a6112eb9d2a943f3ff2401df827018598
An0n3m0us <abdullaahiqbal@hotmail.com>
2019-06-14 16:18:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Blender2Nodebox
+
+Convert cubic Blender models to a Minetest nodebox model. Originally intended to be for making collision/selection boxes for OBJ models, but both can be done.
+
+PLEASE DO NOT SCALE ANY OF THE OBJECTS. Manually make them smaller/bigger rather than scaling them.
+
+![img](https://raw.githubusercontent.com/An0n3m0us/Blender2Nodebox/master/other/img.gif)
+
+# Instructions
+
+1. Create your model
+
+2. Select which objects you want to be exported
+
+3. Press the Run Script button
+
+In the directory of the Blender file, there will be a file called `blender.lua`. The model will be in there.
+
+Here's a ![video](https://raw.githubusercontent.com/An0n3m0us/Blender2Nodebox/master/other/1.mp4) demonstrating how to use it to create a collision/selection box.
---
+++ other/script.py
@@ -0,0 +1,25 @@
+import bpy
+from bpy import context
+
+filepath = bpy.path.abspath("//blender.lua")
+bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
+
+with open(filepath, 'w') as f:
+	for x in context.selected_objects:
+		obj = x
+		mesh = obj.data
+		face1 = obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[3].co*16 + obj.matrix_world*mesh.vertices[6].co*16 + obj.matrix_world*mesh.vertices[7].co*16
+		face1 = str(round(face1[0]/4))
+		face2 = obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[5].co*16 + obj.matrix_world*mesh.vertices[6].co*16
+		face2 = str(round(face2[2]/4))
+		face3 = obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[5].co*16 + obj.matrix_world*mesh.vertices[6].co*16 + obj.matrix_world*mesh.vertices[7].co*16
+		face3 = str(round(face3[1]/4))
+		face4 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[5].co*16
+		face4 = str(round(face4[0]/4))
+		face5 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[3].co*16 + obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[7].co*16
+		face5 = str(round(face5[2]/4))
+		face6 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[3].co*16
+		face6 = str(round(face6[1]/4))
+		faces = "{" + face1 + "/16, " + face2 + "/16, " + face3 + "/16, " + face4 + "/16, " + face5 + "/16, " + face6 + "/16}, -- " + x.name
+		f.write(faces.replace("0/16", "0"))
+		f.write("\n")
