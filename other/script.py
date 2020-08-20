44c77c179ab1de62abbc94ba6f99ab4e98fa61dc
An0n3m0us <an0n3m0us@hotmail.com>
2020-08-20 16:54:05 +0100

    Fix bug

--- other/script.py
+++ other/script.py
@@ -9,17 +9,16 @@ with open(filepath, 'w') as f:
 		obj = x
 		mesh = obj.data
 		face1 = obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[3].co*16 + obj.matrix_world*mesh.vertices[6].co*16 + obj.matrix_world*mesh.vertices[7].co*16
-		face1 = str(round(face1[0]/4))
+		face1 = str(round(face1[0]/4, 1))
 		face2 = obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[5].co*16 + obj.matrix_world*mesh.vertices[6].co*16
-		face2 = str(round(face2[2]/4))
+		face2 = str(round(face2[2]/4, 1))
 		face3 = obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[5].co*16 + obj.matrix_world*mesh.vertices[6].co*16 + obj.matrix_world*mesh.vertices[7].co*16
-		face3 = str(round(face3[1]/4))
+		face3 = str(round(face3[1]/4, 1))
 		face4 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[5].co*16
-		face4 = str(round(face4[0]/4))
+		face4 = str(round(face4[0]/4, 1))
 		face5 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[3].co*16 + obj.matrix_world*mesh.vertices[4].co*16 + obj.matrix_world*mesh.vertices[7].co*16
-		face5 = str(round(face5[2]/4))
+		face5 = str(round(face5[2]/4, 1))
 		face6 = obj.matrix_world*mesh.vertices[0].co*16 + obj.matrix_world*mesh.vertices[1].co*16 + obj.matrix_world*mesh.vertices[2].co*16 + obj.matrix_world*mesh.vertices[3].co*16
-		face6 = str(round(face6[1]/4))
+		face6 = str(round(face6[1]/4, 1))
 		faces = "{" + face1 + "/16, " + face2 + "/16, " + face3 + "/16, " + face4 + "/16, " + face5 + "/16, " + face6 + "/16}, -- " + x.name
-		f.write(faces.replace("0/16", "0"))
 		f.write("\n")
